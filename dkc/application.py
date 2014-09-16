59dbbb1fe86289b496d1f0b9d2f2040511693cc8
Danny Qiu <dqiu55@gmail.com>
2014-09-16 15:48:54 -0400

    Add functionality to update application through ajax

--- dkc/application.py
+++ dkc/application.py
@@ -12,6 +12,23 @@ class ApplicationPage(BaseHandler):
         application = application_key.get()
         template_values = {
             'applicant': applicant,
-            'application': application
+            'application': application,
+            'form_key': application_key.urlsafe()
         }
         self.render_template('application.html', template_values)
+
+    def post(self):
+        application_key = ndb.Key(urlsafe=self.request.get('form-key'))
+        application = application_key.get()
+        applicant = application_key.parent().get()
+
+        application.personal_statement = self.request.get('personal-statement')
+        application.put()
+
+        template_values = {
+            'applicant': applicant,
+            'application': application,
+            'form_key': application_key.urlsafe()
+        }
+        #self.render_template('application.html', template_values)
+        self.response.write(self.request.arguments());
