c6d5c117fd5c976a26801ff248b5262c29685849
Danny Qiu <dqiu55@gmail.com>
2014-11-17 09:40:04 -0500

    Add early submission column to admin overview

--- manage/admin.py
+++ manage/admin.py
@@ -5,8 +5,10 @@ class OverviewHandler(AdminBaseHandler):
 
     def get(self):
         applicants = self.get_applicants()
+        applications = [a.application.get() for a in applicants]
         template_values = {
             'applicants': applicants,
+            'applications': applications,
             'admin_url': '/admin/overview'
         }
         self.render_template('admin-overview.html', template_values)
