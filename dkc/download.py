ad8144e79c8da76efc74f30feeaabd0f1a444a91
Danny Qiu <dqiu55@gmail.com>
2014-11-18 23:38:34 -0500

    Fix handling of pdf access

--- dkc/download.py
+++ dkc/download.py
@@ -1,3 +1,4 @@
+import logging
 from dkc import *
 from dkc.models import User
 from google.appengine.api import users
@@ -7,21 +8,32 @@ import xhtml2pdf.pisa as pisa
 class PDFGeneration(BaseHandler):
 
     def get(self, user_id):
-        if users.get_current_user() or user_id == str(self.user.get_id()):
-            applicant = User.get_by_id(int(user_id))
-            application = applicant.application.get()
-            template_values = {
-                'applicant': applicant,
-                'application': application,
-                'STATIC_DIR': os.path.join(os.path.dirname(__file__), '../static')
-            }
-            template = JINJA_ENVIRONMENT.get_template('pdf/application-pdf.html')
-            html = template.render(template_values)
-            #self.response.write(html)
-            self.response.headers['content-type'] = 'application/pdf'
-            self.response.write(generate_pdf(html))
+        if users.get_current_user():
+            if not users.is_current_user_admin():
+                self.abort(401)
+                return
         else:
-            self.abort(401)
+            if self.user == None: # Prevent access from users no logged in
+                logging.info("Unauthorized access of application %s", user_id)
+                self.abort(401)
+                return
+            elif user_id != str(self.user_info['user_id']): # Prevent access from other users
+                logging.info("Attempted access of application %s by %s", user_id, self.user.email)
+                self.abort(401)
+                return
+
+        applicant = User.get_by_id(int(user_id))
+        application = applicant.application.get()
+        template_values = {
+            'applicant': applicant,
+            'application': application,
+            'STATIC_DIR': os.path.join(os.path.dirname(__file__), '../static')
+        }
+        template = JINJA_ENVIRONMENT.get_template('pdf/application-pdf.html')
+        html = template.render(template_values)
+        #self.response.write(html)
+        self.response.headers['content-type'] = 'application/pdf'
+        self.response.write(generate_pdf(html))
  
 def generate_pdf(html_data):
     html_data = html_data.encode('utf8')
