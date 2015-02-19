b55f0922d46656398eff88a18b1016d816b8f10b
Danny Qiu <dqiu55@gmail.com>
2015-02-19 12:52:42 -0500

    Add indicator for late submission

--- manage/admin.py
+++ manage/admin.py
@@ -1,14 +1,20 @@
 from manage import *
-from dkc.models import User
+from models import *
 import query
 
 class OverviewHandler(AdminBaseHandler):
 
     def get(self):
+        config = ndb.Key(Settings, 'config').get()
+        if not config:
+            config = Settings(id='config')
+        due_date = config.due_date
+
         applicants, applications = query.get_all_overview()
         template_values = {
             'applicants': applicants,
             'applications': applications,
-            'admin_url': '/admin/overview'
+            'admin_url': '/admin/overview',
+            'DUE_DATE': due_date
         }
         self.render_template('admin-overview.html', template_values)
--- manage/show.py
+++ manage/show.py
@@ -1,10 +1,16 @@
 import urllib
 from manage import *
+from models import *
 import query
 
 class ShowHandler(AdminBaseHandler):
 
     def get(self, email):
+        config = ndb.Key(Settings, 'config').get()
+        if not config:
+            config = Settings(id='config')
+        due_date = config.due_date
+
         email = str(urllib.unquote(email))
         applicant, application = query.get_application_by_email(email)
 
@@ -12,7 +18,8 @@ class ShowHandler(AdminBaseHandler):
             'applicant': applicant,
             'application': application,
             'applicant_id': applicant.get_id(),
-            'admin_url': '/admin/show/' + email
+            'admin_url': '/admin/show/' + email,
+            'DUE_DATE': due_date
         }
         self.render_template('admin-show.html', template_values)
 
