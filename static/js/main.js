144fb8165e9e75e913f63f7bf4031c51831776d8
Danny Qiu <dqiu55@gmail.com>
2015-08-06 21:54:06 -0400

    Remove unnecessary encapsulating function and unused css

--- static/js/main.js
+++ static/js/main.js
@@ -14,20 +14,17 @@ function sidebarAffix() {
     }
 }
 
-$(function() {
-    //$('a[href*=#]:not([href=#])').click(function() {
-    $('.home a[href*=#]:not([href=#])').click(function() {
-        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-            if (target.length) {
-                $('html,body').animate({
-                    scrollTop: target.offset().top
-                }, 1000);
-                return false;
-            }
+$('.home a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+            $('html,body').animate({
+                scrollTop: target.offset().top
+            }, 1000);
+            return false;
         }
-    });
+    }
 });
 
 $('.panel-collapse').each(function() {
