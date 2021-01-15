b9abb416503aeeccd1a5c1338641d2364beb76da
Danny Qiu <dqiu55@gmail.com>
2021-01-15 01:20:18 -0800

    Update personal statement page

--- src/dkc/application/personal_statement.py
+++ src/dkc/application/personal_statement.py
@@ -1,45 +1,40 @@
-from flask import render_template
+import logging
+from flask import render_template, request
+from flask_login import current_user, login_required
 from google.cloud import ndb
+from common.models import Settings
 from . import application_bp
-
-# class ApplicationPersonalStatement(BaseHandler):
-
-#     @user_required
-#     def get(self):
-#         self._serve_page()
-
-#     @user_required
-#     def post(self):
-#         application_key = ndb.Key(urlsafe=self.request.get('form-key'))
-#         application = application_key.get()
-
-#         if application.submit_time:
-#             logging.info("Attempt to modify personal statement by %s", applicant.email)
-#             self._serve_page()
-#             return
-
-#         application.personal_statement_choice = self.request.get("personal-statement-choice")
-#         application.personal_statement = self.request.get('personal-statement')
-#         application.put()
-#         self._serve_page()
-
-#     def _serve_page(self):
-#         template_values = {
-#             'application_url': '/application/personal-statement'
-#         }
-#         self.render_application('application-personal_statement.html', template_values)
 
 
 @application_bp.route("/personal-statement", methods=["GET", "POST"])
+@login_required
 def personal_statement():
-    settings = {
-        "due_date": 2020,
-    }
-    applicant = {}
-    application = {}
+    settings = ndb.Key(Settings, "config").get()
+    applicant = current_user
+    application = applicant.application.get()
+
+    if request.method == "POST":
+        handle_post(applicant, application)
+
     template_values = {
-        "settings": settings,
         "applicant": applicant,
         "application": application,
+        "application_url": "/application/personal-statement",
+        "settings": settings,
     }
     return render_template("application/personal-statement.html", **template_values)
+
+
+def handle_post(applicant, application):
+    if application.submit_time:
+        logging.info(
+            "Attempt to modify personal statement by %s after submission",
+            applicant.email,
+        )
+        return
+
+    application.personal_statement_choice = request.form.get(
+        "personal-statement-choice"
+    )
+    application.personal_statement = request.form.get("personal-statement")
+    application.put()
