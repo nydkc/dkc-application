92a4e38c13dc8e9ee78e1eb0e0592dbf0bd859d9
Danny Qiu <dqiu55@gmail.com>
2021-01-15 00:53:44 -0800

    Fix submit check on application

--- src/dkc/application/overview.py
+++ src/dkc/application/overview.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from flask import render_template
 from flask_login import current_user, login_required
 from google.cloud import ndb
@@ -11,9 +12,11 @@ from . import application_bp
 def overview():
     settings = ndb.Key(Settings, "config").get()
     applicant = current_user
+    application = current_user.application.get()
     user_id = current_user.key.id()
     template_values = {
         "applicant": applicant,
+        "application": application,
         "user_id": user_id,
         "application_url": "/application/overview",
         "settings": settings,
