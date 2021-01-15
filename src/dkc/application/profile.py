2d0e98a1db56d1a05fc0f70b432e49284ee36015
Danny Qiu <dqiu55@gmail.com>
2021-01-15 00:35:26 -0800

    Update profile page

--- src/dkc/application/profile.py
+++ src/dkc/application/profile.py
@@ -1,57 +1,56 @@
-from flask import render_template
+import logging
+from flask import render_template, request
+from flask_login import current_user, login_required
 from google.cloud import ndb
+from common.models import Settings
 from . import application_bp
 
-# class ApplicationProfile(BaseHandler):
 
-#     @user_required
-#     def get(self):
-#         self._serve_page()
-
-#     @user_required
-#     def post(self):
-#         applicant = self.user
-#         application = applicant.application.get()
-
-#         if application.submit_time:
-#             logging.info("Attempt to modify profile by %s", applicant.email)
-#             self._serve_page()
-#             return
-
-#         applicant.first_name = self.request.get('first-name')
-#         applicant.last_name = self.request.get('last-name')
-#         applicant.grade = self.request.get('grade')
-#         applicant.address = self.request.get('address')
-#         applicant.city = self.request.get('city')
-#         applicant.zip_code = self.request.get('zip-code')
-#         applicant.phone_number = self.request.get('phone-number')
-#         applicant.division = self.request.get('division')
-#         applicant.ltg = self.request.get('ltg')
-#         applicant.school = self.request.get('school')
-#         applicant.school_address = self.request.get('school-address')
-#         applicant.school_city = self.request.get('school-city')
-#         applicant.school_zip_code = self.request.get('school-zip-code')
-#         applicant.club_president = self.request.get('club-president')
-#         applicant.club_president_phone_number = self.request.get('club-president-phone-number')
-#         applicant.faculty_advisor = self.request.get('faculty-advisor')
-#         applicant.faculty_advisor_phone_number = self.request.get('faculty-advisor-phone-number')
-#         applicant.put()
-#         self._serve_page()
-
-#     def _serve_page(self):
-#         template_values = {
-#             'application_url': '/application/profile'
-#         }
-#         self.render_application('application-profile.html', template_values)
-
-@application_bp.route('/profile', methods=['GET', 'POST'])
+@application_bp.route("/profile", methods=["GET", "POST"])
+@login_required
 def profile():
-    settings = {
-        "due_date": 2020,
-    }
-    applicant = {}
+    settings = ndb.Key(Settings, "config").get()
+    applicant = current_user
+    application = applicant.application.get()
+
+    if request.method == "POST":
+        handle_post(applicant, application)
+
     template_values = {
+        "applicant": applicant,
+        "application": application,
+        "application_url": "/application/profile",
         "settings": settings,
-        "applicant": applicant
     }
-    return render_template('application/profile.html', **template_values)
+    return render_template("application/profile.html", **template_values)
+
+
+def handle_post(applicant, application):
+    if application.submit_time:
+        logging.info(
+            "Attempt to modify profile by %s after submission", applicant.email
+        )
+        return
+
+    applicant.first_name = request.form.get("first-name")
+    applicant.last_name = request.form.get("last-name")
+    application.grade = request.form.get("grade")
+    application.address = request.form.get("address")
+    application.city = request.form.get("city")
+    application.zip_code = request.form.get("zip-code")
+    application.phone_number = request.form.get("phone-number")
+    application.division = request.form.get("division")
+    application.ltg = request.form.get("ltg")
+    application.school = request.form.get("school")
+    application.school_address = request.form.get("school-address")
+    application.school_city = request.form.get("school-city")
+    application.school_zip_code = request.form.get("school-zip-code")
+    application.club_president = request.form.get("club-president")
+    application.club_president_phone_number = request.form.get(
+        "club-president-phone-number"
+    )
+    application.faculty_advisor = request.form.get("faculty-advisor")
+    application.faculty_advisor_phone_number = request.form.get(
+        "faculty-advisor-phone-number"
+    )
+    ndb.put_multi([applicant, application])
