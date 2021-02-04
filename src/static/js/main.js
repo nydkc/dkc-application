d8b420101d31049a511be2cf0752568e3c13449c
Danny Qiu <dqiu55@gmail.com>
2021-02-04 02:50:23 -0800

    Update to latest versions of JQuery v1.12.4 and Bootstrap v3.4.1, with local fallback

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -14,7 +14,7 @@ function sidebarAffix() {
     }
 }
 
-$('.home a[href*=#]:not([href=#])').click(function() {
+$('.home a[href*=\\#]:not([href=\\#])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
         var target = $(this.hash);
         target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
