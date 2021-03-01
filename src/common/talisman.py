060c4cb0a69dc0330e5dc33579e78cf5c7a4a8d1
Danny Qiu <dqiu55@gmail.com>
2021-03-01 01:57:25 -0800

    Add HTTP bypass for GAE to cron endpoint

--- src/common/talisman.py
+++ src/common/talisman.py
@@ -1,6 +1,18 @@
 import os
+from flask import request
 from flask_talisman import Talisman
 
 
+class TalismanWithCronHttpsBypass(Talisman):
+    def _force_https(self):
+        """
+        Override the default _force_https to handle GAE cron request, which
+        uses HTTP and treats the HTTPS 302 redirect as a failure.
+        """
+        if request.path.startswith("/cron"):
+            return
+        return super()._force_https()
+
+
 def g_flask_talisman_init_app(app):
-    Talisman(app, content_security_policy=None)
+    TalismanWithCronHttpsBypass(app, force_https=True, content_security_policy=None)
