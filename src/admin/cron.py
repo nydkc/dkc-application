448bd65abada72553e90f936b8178b9ed559fc3b
Danny Qiu <dqiu55@gmail.com>
2023-02-06 18:13:36 -0800

    Add cron job to send a dummy email every month using SendGrid

--- src/admin/cron.py
+++ src/admin/cron.py
@@ -4,6 +4,8 @@ from datetime import datetime, timedelta
 from flask import abort, Blueprint, request
 from google.cloud import ndb
 from common.constants import AUTH_TOKEN_VALIDITY_DAYS
+from common.email_provider import SendGrid, Email, Subject, HtmlContent, CustomArgs
+from common.models import Settings
 from dkc.auth.models import AuthToken
 from admin.auth.models import AdminUser
 
@@ -44,4 +46,32 @@ def admin_user_token_cleanup():
     ]
     ndb.delete_multi(admin_user_token_keys)
     logger.info("Cleaned up %d admin user tokens", len(admin_user_token_keys))
+    return "ok"
+
+
+@cron_bp.route("/cron/sendgrid_heartbeat")
+def sendgrid_heartbeat():
+    if not request.headers.get(APPENGINE_CRON_HEADER, default=False):
+        return abort(403)
+
+    settings = ndb.Key(Settings, "config").get()
+    sg = SendGrid(api_key=settings.sendgrid_api_key)
+    response = sg.send_email(
+        from_email=Email(email="dkc-app@nydkc.org", name="NYDKC DKC Application"),
+        to_email=Email(email="dkc-app@nydkc.org"),
+        subject=Subject(line="DKC Application is still alive!"),
+        html_content=HtmlContent(
+            content="Hello! This email keeps the SendGrid account active."
+        ),
+        custom_args=CustomArgs(
+            metadata=(
+                {
+                    "dkc_purpose": "heartbeat",
+                }
+            )
+        ),
+    )
+    if response.http_code != 202:
+        logger.error("Error sending heartbeat email")
+        return abort(503)
     return "ok"
